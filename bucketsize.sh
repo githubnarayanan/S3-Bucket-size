dd7b99df5857234578046f11062fda4894366219
githubnarayanan <jithutnarayanan@gmail.com>
2023-01-30 06:47:33 +0530

    error fix

--- bucketsize.sh
+++ bucketsize.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/bash
+#!/usr/bin/bash 
 set +x
 PROFILE=default
 function calcs3bucketsize() {
-    sizeInBytes=aws --profile ${PROFILE} s3 ls s3://"${1}" --recursive --human-readable --summarize | awk END'{print}'
-    echo ${1},${sizeInBytes} >> buckets-sizes.csv
-    printf "DONE. Size of the bucket ${1}. %s\n " "${sizeInBytes}"
+    sizeInBytes=`aws --profile ${PROFILE} s3 ls s3://"${1}" --recursive --human-readable --summarize | awk END'{print}'`
+    echo ${1},${sizeInBytes} >> buckets-s3-sizes.csv
+    printf "DONE. Size of the bucket ${1}. %s\n " "${sizeInBytes}"  
 }
-[ -f buckets-sizes.csv ] && rm -fr buckets-sizes.csv
-buckets=aws --profile ${PROFILE}  s3 ls | awk '{print $3}'
+[ -f buckets-s3-sizes.csv ] && rm -fr buckets-s3-sizes.csv
+buckets=`aws --profile ${PROFILE}  s3 ls | awk '{print $3}'`
 i=1
 for j in ${buckets}; do
-    printf "calculating the size of the bucket[%s]=%s. \n " "${i}" "${j}"
+    printf "calculating the size of the bucket[%s]=%s. \n " "${i}" "${j}"   
     i=$((i+1))
     # to expedite the calculation, make the cli commands run parallel in the background
     calcs3bucketsize ${j} &
